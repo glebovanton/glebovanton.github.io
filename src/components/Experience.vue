6d6d7768f58dff2d1a514a385c9c4caee669d05d
Anton <workglebov@gmail.com>
2024-07-30 13:59:46 +0300

    move npm

---
+++ src/components/Experience.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { experiencesData } from "@/data";
+</script>
+
+<template>
+  <section id="experience" class="scroll-mt-28 mb-28 sm:mb-40">
+    <h2 class="text-3xl font-medium capitalize mb-8 text-center">
+      My experience
+    </h2>
+    <div
+      class="vertical-timeline vertical-timeline--animate vertical-timeline--two-columns"
+    >
+      <div
+        v-for="(experience, index) in experiencesData"
+        :key="`experience-${index}`"
+        class="vertical-timeline-element"
+      >
+        <span
+          style="background: rgba(255, 255, 255, 0.15); font-size: 1.5rem"
+          class="vertical-timeline-element-icon bounce-in"
+        >
+          <component :is="experience.icon"></component>
+        </span>
+        <div
+          style="
+            background: rgba(255, 255, 255, 0.05);
+            box-shadow: none;
+            border: 1px solid rgba(0, 0, 0, 0.05);
+            text-align: left;
+            padding: 1.3rem 2rem;
+          "
+          class="vertical-timeline-element-content bounce-in"
+        >
+          <div
+            style="border-right: 0.4rem solid rgba(255, 255, 255, 0.5)"
+            class="vertical-timeline-element-content-arrow"
+          ></div>
+          <h3 class="font-semibold capitalize">{{ experience.title }}</h3>
+          <p class="font-normal !mt-0">{{ experience.location }}</p>
+          <p
+            class="!mt-1 !font-normal text-gray-700 dark:text-white/75"
+            v-html="experience.description"
+          ></p>
+          <span class="vertical-timeline-element-date">{{
+            experience.date
+          }}</span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/experienceStyles.scss";
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { skillsData } from "@/data";
+</script>
+
+<template>
+  <section
+    id="skills"
+    class="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+  >
+    <h2 class="text-3xl font-medium capitalize mb-8 text-center">My skills</h2>
+    <ul class="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+      <li
+        v-for="skill in skillsData"
+        :key="`skills-${skill}`"
+        class="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+        style="opacity: 1; transform: none"
+      >
+        {{ skill }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style scoped></style>
